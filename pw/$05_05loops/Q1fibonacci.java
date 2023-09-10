23575583a8202737490472bbe1f489852c907227
Vishal Khatavkar <vishalkhatavkar2000@gmail.com>
2023-09-10 18:30:42 +0530

    chenged fibonacci output

--- pw/$05_05loops/Q1fibonacci.java
+++ pw/$05_05loops/Q1fibonacci.java
@@ -11,7 +11,7 @@ public class Q1fibonacci {
 
         int n = sc.nextInt(), a = 1, b = 1, c;
         System.out.print(a + " " + b + " ");
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             c = a + b;
             a = b;
             b = c;
